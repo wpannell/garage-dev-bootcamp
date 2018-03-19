2bd61e4c52fe2a004508e86660d7f3e7fb29dd3f
WIL PANNELL <wgpannel@us.ibm.com>
2018-03-18 19:16:10 -0500

    format for legibility

--- config/webpack.test.browser.js
+++ config/webpack.test.browser.js
@@ -5,34 +5,34 @@ const commonTestConfig = require('./webpack.test.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = webpackMerge(commonTestConfig, {
-    devtool: 'cheap-module-inline-source-map',
+  devtool: 'cheap-module-inline-source-map',
 
-    target: 'web',
+  target: 'web',
 
-    entry: {
-        'test': 'mocha-loader!./config/mocha/mocha-browser-test-shim.js'
-    },
+  entry: {
+    'test': 'mocha-loader!./config/mocha/mocha-browser-test-shim.js'
+  },
 
-    output: {
-        path: helpers.root('tests'),
-        publicPath: '/',
-        filename: 'test.bundle.js'
-    },
+  output: {
+    path: helpers.root('tests'),
+    publicPath: '/',
+    filename: 'test.bundle.js'
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                include: helpers.root('src'),
-                exclude: [/\.e2e\.ts$/],
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
-            }
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: helpers.root('src/mocha-browser-test/mocha-index.html')
-        })
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: helpers.root('src'),
+        exclude: [/\.e2e\.ts$/],
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      }
     ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: helpers.root('src/mocha-browser-test/mocha-index.html')
+    })
+  ]
 });
